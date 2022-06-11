62b7c31d2678f990b892ec17aa0227e4d278217a
Rachel Bock <rachel.online@gmail.com>
2022-06-11 14:46:53 -0600

    Add new banner, add pull request template, and add DayJS 3rd party library

--- src/scripts.js
+++ src/scripts.js
@@ -14,6 +14,7 @@ import Trips from './Trips';
 
 import './images/turing-logo.png'
 import './images/map-banner.png'
+import './images/mapBanner.png'
 import './images/one.jpg'
 import './images/two.jpg'
 import './images/three.jpg'
@@ -31,7 +32,7 @@ const getRandomID = () => {
 
 const travelerID = getRandomID();
 
-function getData() {
+const getData = () => {
   promise.then(data => {
     travelers = data[0];
     trips = data[1];
